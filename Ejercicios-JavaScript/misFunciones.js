e1befd4d230387f0602cdf4b8e302eb3330390d3
romanovictoria <vicctoriarom@gmail.com>
2022-05-20 12:19:41 -0300

    se realizo ejercicio de grados-radianes que no me salió bien

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -30,3 +30,16 @@ function cambiarUnidades (id,valor){
         document.lasUnidades.unid_pulgada.value = 12*valor;
     }
 }
+function convertirGR (id){
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }else if (id=="radianes"){
+        rad = document.getElementById(elementId:"radianes"). value;
+        grad = (rad*180)/Math.PI
+    }
+    document.getElementById( elementId: "grados") .value = grad;
+    document.getElementById( elementId: "radianes") .value = rad;
+}
+}
